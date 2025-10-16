18c8acfa83afa3881e0f55c268e8f3ac68553d40
Pixel-Quark <fizagilani05@gmail.com>
2025-10-16 21:32:47 +0500

    Initial commit

--- paradise-nursery/src/App.jsx
+++ paradise-nursery/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./Navbar";
 import LandingPage from "./pages/LandingPage";
 import ProductListing from "./pages/ProductListing";
