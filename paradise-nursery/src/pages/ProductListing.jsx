42c3611a5afcc70028ae89ff124a4573c7d17598
Pixel-Quark <fizagilani05@gmail.com>
2025-10-16 21:49:24 +0500

    Initial commit

--- paradise-nursery/src/pages/ProductListing.jsx
+++ paradise-nursery/src/pages/ProductListing.jsx
@@ -13,22 +13,22 @@ const ProductListing = () => {
             {
               name: "Lavender",
               desc: "Soothing fragrance for calmness.  10$",
-              img: "paradise-nursery/src/assets/True-Lavender-plant.webp",
+              img: "paradise-nursery/public/True-Lavender-plant.webp",
             },
             {
               name: "Rosemary",
               desc: "Aromatic herb for your kitchen and mind.  10$",
-              img: "paradise-nursery/src/assets/rosemary.jpg",
+              img: "paradise-nursery/public/assets/rosemary.jpg",
             },
             {
               name: "Mint",
               desc: "Refreshing aroma, perfect for tea and drinks.  10$",
-              img: "paradise-nursery/src/assets/mint plant.jpg",
+              img: "paradise-nursery/public/assets/mint plant.jpg",
             },
             {
               name: "Jasmine",
               desc: "Delicate and sweet scent loved by all.  10$",
-              img: "paradise-nursery/src/assets/jasmine.jpg",
+              img: "paradise-nursery/public/assets/jasmine.jpg",
             },
           ].map((p) => (
             <PlantCard key={p.name} {...p} />
@@ -45,22 +45,22 @@ const ProductListing = () => {
             {
               name: "Aloe Vera",
               desc: "Natural healer for skin and burns.  10$",
-              img: "paradise-nursery/src/assets/aloe vera.jpg",
+              img: "paradise-nursery/public/assets/aloe vera.jpg",
             },
             {
               name: "Tulsi (Holy Basil)",
               desc: "Sacred herb with multiple health benefits.   10$",
-              img: "paradise-nursery/src/assets/tulsi.png",
+              img: "paradise-nursery/public/assets/tulsi.png",
             },
             {
               name: "Neem",
               desc: "Powerful medicinal tree for detoxifying.   10$",
-              img: "paradise-nursery/src/assets/neem.webp",
+              img: "paradise-nursery/public/assets/neem.webp",
             },
             {
               name: "Ashwagandha",
               desc: "Stress-relieving plant used in Ayurveda.    10$",
-              img: "paradise-nursery/src/assets/ashwagandha plant.jpg",
+              img: "paradise-nursery/public/assets/ashwagandha plant.jpg",
             },
           ].map((p) => (
             <PlantCard key={p.name} {...p} />
