a43478b529322507298749af3eb854d0f20d928d
Pixel-Quark <fizagilani05@gmail.com>
2025-10-19 00:29:10 +0500

    Initial commit

--- paradise-nursery/vite.config.js
+++ paradise-nursery/vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/nursery-app/' // <-- replace with your repo name exactly
+  base: './' // <-- relative paths for Netlify
 })
